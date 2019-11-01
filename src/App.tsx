85e4afa61507806e3e50a1a2daee76626c785cce
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-11-01 20:33:26 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const randomColorValue = () => Math.round(Math.random() * 255);
+const randomRgb = () => `rgb(${randomColorValue()}, ${randomColorValue()}, ${randomColorValue()})`;
+
+const getColors = (): string[] => {
+  return new Array(10000).fill('').map(randomRgb);
+}
+
+interface State {
+  colors: string[];
+}
+
+const Square: React.FC<{ color: string, index: number }> = ({ color, index }) => (
+  <div style={{ backgroundColor: color }} className="square">{index}</div>
+);
+
+class App extends React.PureComponent<{}, State> {
+  state: State = {
+    colors: []
+  };
+  private colors: string[] = getColors();
+  private currentChunk: number = 1;
+  private readonly chunkSize: number = 3;
+
+  componentDidMount() {
+    this.setNext();
+  }
+
+  private setNext(): void {
+    this.setState(
+      prevState => ({ colors: prevState.colors.concat(this.nextChunk) }),
+      () => this.scheduleNext()
+    );
+  }
+
+  private scheduleNext(): void {
+    if (this.currentChunk * this.chunkSize > this.colors.length) {
+      return;
+    }
+    window.setTimeout(() => {
+      this.currentChunk++;
+      this.setNext();
+    }, 500);
+  }
+
+  private get nextChunk(): string[] {
+    const startPoint = this.chunkSize * this.currentChunk;
+    return this.colors.slice(startPoint, startPoint + this.chunkSize);
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        {this.state.colors.map((color, index) => (
+          <Square color={color} key={index} index={index} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
